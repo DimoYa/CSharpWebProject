7f4f2cea59c9fdacca793f8dc9c0e4809e5ec0f3
DimoYa <dimo_yanev@abv.bg>
2019-07-03 00:34:02 +0300

    Add more design changes related to responsivness. Seed user roles on initial start of app

--- MyResourcePlanning/MyResourcePlanning.Common/GlobalConstants.cs
+++ MyResourcePlanning/MyResourcePlanning.Common/GlobalConstants.cs
@@ -3,5 +3,9 @@
     public static class GlobalConstants
     {
         public const string AdministratorRoleName = "Administrator";
+
+        public const string PlannerRoleName = "Planner";
+
+        public const string ApproverRoleName = "Approver";
     }
 }
