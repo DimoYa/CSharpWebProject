df3933af6ff70369b96f1a0f9b0c416bf80af891
Dimo Yanev <dimoyanev@kpmg.com>
2019-07-03 14:44:07 +0300

    Implement Requests controller basic logic and view all requests

---
+++ MyResourcePlanning/Web/MyResourcePlanning.Web/Controllers/RequestsController.cs
@@ -0,0 +1,32 @@
+﻿namespace MyResourcePlanning.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class RequestsController : BaseController
+    {
+        public RequestsController()
+        {
+        }
+
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(/*EventCreateBindingModel bindingModel*/string test)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.Redirect("/Requests/Create");
+            }
+
+            return this.RedirectToAction("All");
+        }
+
+        public IActionResult All()
+        {
+            return this.View();
+        }
+    }
+}
