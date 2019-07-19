10d8ffdebf42468a599fe31e8d15d0bacda31898
DimoYa <dimo_yanev@abv.bg>
2019-07-19 19:40:05 +0300

    UI improvements on validation messages

--- MyResourcePlanning/Web/MyResourcePlanning.Web/wwwroot/js/wrapTextInASpan.js
+++ MyResourcePlanning/Web/MyResourcePlanning.Web/wwwroot/js/wrapTextInASpan.js
@@ -1,7 +1,7 @@
 ﻿function wrapInASpan(text, className) {
 
     text.forEach(function (item) {
-        $(`span, li:contains('${item}')`).html(function (_, html) {
+        $(`span, ul, li:contains('${item}')`).html(function (_, html) {
             return html.replace(item, `<span class="${className}">${item}</span>`);
         });
     });
