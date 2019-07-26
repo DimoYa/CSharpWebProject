fd22f3e08a11c2b5061ed02ad7215c04c2ffa1d0
Yanev, Dimo <dyanev@kpmg.com>
2019-07-26 04:16:44 +0300

    add delete confirmation modal window and move some js logic to separate class

---
+++ MyResourcePlanning/Web/MyResourcePlanning.Web/wwwroot/js/addGrouping.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    $(".collapse.show").each(function () {
+        $(this).prev(".panel-heading").find(".far").addClass("fa-object-group").removeClass("fa-object-ungroup");
+    });
+
+    $(".collapse").on('show.bs.collapse', function () {
+        $(this).prev(".panel-heading").find(".far").removeClass("fa-object-ungroup").addClass("fa-object-group");
+    }).on('hide.bs.collapse', function () {
+        $(this).prev(".panel-heading").find(".far").removeClass("fa-object-group").addClass("fa-object-ungroup");
+    });
+});
